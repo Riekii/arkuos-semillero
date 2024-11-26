29df23714850473452c224567d05d74c783c7f9e
Manuel Junquera <manuel.junquera@btponetec.es>
2024-11-26 16:51:13 +0100

    Listado seeds

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { DescriptionComponent } from './description/description.component';
 import { AboutComponent } from './about/about.component';
+import { SeedModalComponent } from './seed/seed-modal/seed-modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, DescriptionComponent, AboutComponent],
+  imports: [HeaderComponent, DescriptionComponent, AboutComponent, SeedModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/home/seed/seed-modal/seed-modal.component.ts
+++ src/app/home/seed/seed-modal/seed-modal.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FirebaseService } from '../../../service/firebase.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-seed-modal',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './seed-modal.component.html',
   styleUrl: './seed-modal.component.scss'
 })
@@ -12,7 +14,7 @@ export class SeedModalComponent implements OnInit{
 
   fire = inject(FirebaseService);
 
-  public seeds$: any;
+  public seeds!: any[];
 
   ngOnInit(): void {
     this.getSeeds();
@@ -21,6 +23,7 @@ export class SeedModalComponent implements OnInit{
   getSeeds(){
     this.fire.getSeeds().subscribe((resp: any) => {
       console.log(resp)
+      this.seeds = resp;
     })
   }
 }
