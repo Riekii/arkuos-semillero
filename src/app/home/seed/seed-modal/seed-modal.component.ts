69925cea20bb8d5ed6c1119e54340cbe03f83265
Rieki <riekikeika@gmail.com>
2024-11-27 18:05:48 +0100

    PRUEBAS

--- src/app/home/seed/seed-modal/seed-modal.component.ts
+++ src/app/home/seed/seed-modal/seed-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FirebaseService } from '../../../service/firebase.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, timeout } from 'rxjs';
 
 @Component({
   selector: 'app-seed-modal',
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
   templateUrl: './seed-modal.component.html',
   styleUrl: './seed-modal.component.scss'
 })
-export class SeedModalComponent implements OnInit{
+export class SeedModalComponent implements OnInit {
 
   fire = inject(FirebaseService);
 
@@ -20,10 +20,51 @@ export class SeedModalComponent implements OnInit{
     this.getSeeds();
   }
 
-  getSeeds(){
+  getSeeds() {
     this.fire.getSeeds().subscribe((resp: any) => {
-      console.log(resp)
       this.seeds = resp;
+      this.prueba();
+
     })
+  }
+
+
+  prueba() {
+    for (
+      let index = 0;
+      index < this.seeds.length;
+      index++) {
+      console.warn('SEMILLAS', index)
+      const element = this.seeds[index];
+    }
+
+    if (this.seeds.length === 4) {
+      console.log('HAY 4 SEMILLAS')
+    } else {
+      console.log('JAJA')
+    }
+
+    switch (this.seeds.length) {
+      case 1:
+        // solo hay 1
+        break;
+      case 2:
+
+        break;
+      case 3:
+
+        break;
+      case 4:
+
+        break;
+
+      default:
+        console.warn('hay demasiadas')
+        break;
+    }
+
+    setTimeout(() => {
+      console.error('BOOOM')
+    }, 6000);
   }
 }
