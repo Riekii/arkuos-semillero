ee9a5482ece7c9749f04050838c62850aff9160b
Rieki <riekikeika@gmail.com>
2024-12-06 12:30:57 +0100

    Sol

--- src/app/animations/animation-sun/animation-sun.component.ts
+++ src/app/animations/animation-sun/animation-sun.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'sun',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './animation-sun.component.html',
   styleUrl: './animation-sun.component.scss'
 })
+
+
 export class AnimationSunComponent {
 
+  public sunActive: boolean = true;
+
+  private lastScrollTop = 0;
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event: any) {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScrollTop < this.lastScrollTop) {
+    // console.log('Scroll hacia a´rriba');
+  } else if (currentScrollTop > this.lastScrollTop) {
+    // console.log('Scroll hacia abajo');
+  }
+  
+  if(currentScrollTop < 400) this.sunActive = true;
+  else this.sunActive = false;
+
+  this.lastScrollTop = currentScrollTop;
+
+
+  
+}
 }
--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SeedModalComponent } from '../seed/seed-modal/seed-modal.component';
 import { AnimationSeedComponent } from '../../animations/animation-seed/animation-seed.component';
+import { SeparadorComponent } from '../../shared/separador/separador.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SeedModalComponent, AnimationSeedComponent],
+  imports: [SeedModalComponent, AnimationSeedComponent, SeparadorComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
