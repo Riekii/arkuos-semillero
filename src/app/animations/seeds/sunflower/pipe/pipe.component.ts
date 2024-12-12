8ccac6f9557bcfa28d9423ed0ef62e85e198b8e6
Manuel Junquera <manuel.junquera@btponetec.es>
2024-12-12 16:56:11 +0100

    pipas

---
+++ src/app/animations/seeds/sunflower/pipe/pipe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sun-pipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './pipe.component.html',
+  styleUrl: './pipe.component.scss'
+})
+export class PipeComponent implements OnInit{
+  ngOnInit(): void {
+    // let mMax = '--mMax: ' + this.popCorn.mMax + '; ';
+    // let mMax = '--mMax: ' + this.popCorn.mMax + '; ';
+
+  }
+}
--- src/app/animations/seeds/sunflower/sunflower.component.ts
+++ src/app/animations/seeds/sunflower/sunflower.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AnimationPotComponent } from '../../animation-pot/animation-pot.component';
+import { CommonModule } from '@angular/common';
+import { PipeComponent } from './pipe/pipe.component';
+import { SharedService } from '../../../service/shared.service';
 
 @Component({
   selector: 'seed-sunflower',
   standalone: true,
-  imports: [AnimationPotComponent],
+  imports: [AnimationPotComponent, CommonModule, PipeComponent],
   templateUrl: './sunflower.component.html',
   styleUrl: './sunflower.component.scss'
 })
-export class SunflowerComponent {
+export class SunflowerComponent implements OnInit{
+
+  constructor(
+    public shared: SharedService
+  ){}
+
+  public flowers: any = [];
+  public pipes: any = [];
+
+  ngOnInit(): void {
+    for (let index = 0; index < 8; index++) {
+      this.flowers.push({
+        flower: 'yes :)'
+      })
+      this.pipes.push ({
+        mMax: this.shared.getRandomInt(-100, 100) + 'vh',
+        lMax: this.shared.getRandomInt(-100, 100) + 'vw',
+      })
+      
+    }
+  }
+
   public sunflowerStart(){
 
   }
